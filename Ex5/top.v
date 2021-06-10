e8496bddc13cdefb20006679f4bcfad83fee5ec3
Cloud User <centos@ip-172-31-8-110.eu-west-2.compute.internal>
2021-06-10 10:15:49 +0000

    A complete solution to Ex5

--- Ex5/top.v
+++ Ex5/top.v
@@ -13,3 +13,36 @@
 //           heating, cooling
 //////////////////////////////////////////////////////////////////////////////////
 
+
+`timescale 1ns/100ps
+
+module AC (
+
+
+     //adding ports
+      input clk,
+      input [4:0]temp,
+      output heating,
+      output cooling  
+);
+
+
+      reg [1:0]state;  //or output reg [1:0]state;
+                      //2-bit state machine
+
+
+      //let 10-heating, 00-idle, 01-cooling, 11 is not possible
+       assign heating = state[1];
+       assign cooling = state[0];
+    
+
+       always @(posedge clk) begin
+            case(state)
+               2'b10: state = temp<20 ? 2'b10 : 2'b00;     //when heating is on - if temp<20 then keeps      heating otherwise turns to idle                  
+               2'b00: state = temp<=18 ? 2'b10 : temp>=22 ? 2'b01 : 2'b00;    //when in idle state - if temp<=18 turns heating on and when temp>=22 turns cooling on, otherwise stays in idlea state
+               2'b01: state = temp>20 ? 2'b01 : 2'b00;     //when cooling is on - if temp>20 keeps cooling otherwise turns to idle
+               default: state = 2'b00;     //set default state as idle
+            endcase
+       end
+endmodule
+      
--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -7,3 +7,72 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+
+`timescale 1ns / 100ps
+
+module top_tb(
+    );
+    
+     //Parameters
+      parameter CLK_PERIOD = 10;
+
+
+     //Regitsers and wires
+       //From the module
+        reg clk; 
+        reg [4:0]temp;
+        wire heating, cooling;
+       //For testing purposes
+        reg err;
+
+
+     //Clock generation
+       initial
+       begin
+          clk = 1'b1;
+          forever
+          #(CLK_PERIOD/2) clk=~clk;
+       end
+
+
+     //Testing output
+         initial begin
+         err=0;
+
+            forever begin
+               #CLK_PERIOD
+               temp = $urandom_range(30,10);    //use the random function to generate a random temperature
+               #(CLK_PERIOD*3)                  //give the system some time to reach steady state
+               if(temp<18 && heating!=1) begin
+                  $display("***TEST FAILED 1***");
+                  err = 1;
+               end
+               else if(temp>22 && cooling!=1) begin
+                  $display("***TEST FAILED 2***");
+                  err = 1;
+               end
+               else if(heating==1 && cooling==1) begin
+                  $display("***TEST FAILED 3***");
+                  err = 1;
+               end 
+            end
+         end
+          
+
+     //Finish test, check for success
+         initial begin
+             #1000 
+             if (err==0)
+                $display("***TEST PASSED! :) ***");
+                $finish;
+         end
+
+
+     //Instantiate counter module
+      AC top(
+      .clk(clk),
+      .temp(temp),
+      .heating(heating),
+      .cooling(cooling)
+     );
+endmodule 
