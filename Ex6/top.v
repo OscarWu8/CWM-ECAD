40fbe603d0dc6fea033d4d8ec89abe75ce768e9d
Cloud User <centos@ip-172-31-8-110.eu-west-2.compute.internal>
2021-06-10 15:12:16 +0000

    A complete solution to Ex6

--- Ex6/top.v
+++ Ex6/top.v
@@ -15,3 +15,32 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 
+
+`timescale 1ns / 100ps
+
+module RGB_CC (
+
+    //add ports
+     input clk,
+     input enable,
+     input [2:0]colour,
+     output [23:0]rgb
+    );
+
+    //no extra wires or regs needed
+
+    //Instantiating the core
+     mybram rgb_mem (
+      .clka(clk),    // input wire clka
+      .ena(enable),      // input wire ena
+      .wea(1'b0),      // input wire [0 : 0] wea
+      .addra(colour),  // input wire [2 : 0] addra
+      .dina(24'h0),    // input wire [23 : 0] dina
+      .douta(rgb)  // output wire [23 : 0] douta
+     );
+
+   
+endmodule
+
+
+
--- Ex6/top_tb.v
+++ Ex6/top_tb.v
@@ -7,3 +7,81 @@
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
 
+
+
+
+`timescale 1ns / 100ps
+
+module top_tb(
+    );
+    
+     //Parameters
+      parameter CLK_PERIOD = 10;
+
+
+     //Regitsers and wires
+       //From the module
+        reg clk; 
+        reg enable;
+        reg [2:0]colour;
+        wire [23:0]rgb;
+       //For testing purposes
+        reg err;
+        reg [23:0]rgb_prev;
+
+
+     //Clock generation
+       initial
+       begin
+          clk = 1'b1;
+          forever
+          #(CLK_PERIOD/2) clk=~clk;
+       end
+
+
+     //Testing output
+         initial begin
+         enable=1;
+         colour=0;
+         rgb_prev=0;
+
+         err=0;
+            forever begin
+               enable=1;       //test if the converter changes states when enable=1
+               rgb_prev = rgb;
+               colour = colour + 1;
+               #(CLK_PERIOD*3)          //give the system some time to change states
+               if(rgb_prev==rgb) begin
+               $display("***TEST FAILED, RGB not changing***");
+                  err = 1;
+               end  
+                                      
+               rgb_prev=rgb;
+               enable=0;      //test if the converter stays in the same state when enable=0
+               #(CLK_PERIOD*3)          //give the system some time to change state
+               if(rgb_prev!=rgb) begin
+                  $display("***TEST FAILED, RGB should not be changing***");
+                  err = 1;
+               end
+            end
+         end
+          
+
+     //Finish test, check for success
+         initial begin
+             #500 
+             if (err==0)
+                $display("***TEST PASSED! :) ***");
+                $finish;
+         end
+
+
+     //Instantiate counter module
+      RGB_CC top(
+      .clk(clk),
+      .enable(enable),
+      .colour(colour),
+      .rgb(rgb)
+     );
+endmodule 
+
