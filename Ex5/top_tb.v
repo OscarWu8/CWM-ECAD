82e77e908e939787c5e145b8593e03c66ffc5529
Cloud User <centos@ip-172-31-8-110.eu-west-2.compute.internal>
2021-06-11 12:37:55 +0000

    A complete solution to Ex5

--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -22,8 +22,13 @@ module top_tb(
         reg clk; 
         reg [4:0]temp;
         wire heating, cooling;
+        wire [1:0]state;
        //For testing purposes
         reg err;
+        reg [1:0]state_prev;
+
+        assign state[1] = heating;
+        assign state[0] = cooling;
 
 
      //Clock generation
@@ -36,32 +41,44 @@ module top_tb(
 
 
      //Testing output
-         initial begin
-         err=0;
+      initial begin
+        temp = 5'd15;      //set temperature to an initial value
+        err=0;
+        state_prev = state;
 
             forever begin
-               #CLK_PERIOD
-               temp = $urandom_range(30,10);    //use the random function to generate a random temperature
-               #(CLK_PERIOD*3)                  //give the system some time to reach steady state
+               #(CLK_PERIOD*3)       //give the system some time to reach steady state
                if(temp<18 && heating!=1) begin
                   $display("***TEST FAILED 1***");
                   err = 1;
                end
-               else if(temp>22 && cooling!=1) begin
+
+               if(temp>22 && cooling!=1) begin
                   $display("***TEST FAILED 2***");
                   err = 1;
                end
-               else if(heating==1 && cooling==1) begin
+
+               if(state_prev==2'b00 && state==2'b00) begin
+                  if(temp<=18 || temp>=22) begin
                   $display("***TEST FAILED 3***");
                   err = 1;
+                  end 
+               end
+
+               if(heating==1 && cooling==1) begin
+                  $display("***TEST FAILED 4***");
+                  err = 1;
                end 
+
+               state_prev = state;
+               temp = temp + 1;     //increase temperature by one each time
             end
-         end
+      end
           
 
      //Finish test, check for success
          initial begin
-             #1000 
+             #200 
              if (err==0)
                 $display("***TEST PASSED! :) ***");
                 $finish;
